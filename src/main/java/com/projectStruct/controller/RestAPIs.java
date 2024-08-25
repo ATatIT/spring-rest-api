b717361494a5b6b5c7ca0a0a428edfa44feba2aa
Abhishek <athesiya37@gmail.com>
2024-08-25 14:33:11 +0530

    do content negotiation

--- src/main/java/com/projectStruct/controller/RestAPIs.java
+++ src/main/java/com/projectStruct/controller/RestAPIs.java
@@ -1,9 +1,11 @@
 package com.projectStruct.controller;
 
-
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,53 +23,51 @@ import jakarta.validation.Valid;
 
 @RestController
 public class RestAPIs {
-	
+
 	userDao userService;
-	
-	
-	public RestAPIs(userDao userService) {
+	private MessageSource massageSource;
+
+	public RestAPIs(userDao userService, MessageSource massageSource) {
 		super();
 		this.userService = userService;
+		this.massageSource = massageSource;
 	}
 
-	@GetMapping("/hello")
-	public  String helloUser() {
-		return "Hello User";
-	}
-	
+
 	@GetMapping("/users")
-	public List<UserBean> allUser(){
+	public List<UserBean> allUser() {
 		return userService.findAllUser();
 	}
-	
+
 	@GetMapping("/users/{userId}")
 	public UserBean userById(@PathVariable int userId) {
-		UserBean user = userService.findUserById(userId); 
-		if(user == null)
-			throw new UserNotFoundException("id:"+ userId);
-		
+		UserBean user = userService.findUserById(userId);
+		if (user == null)
+			throw new UserNotFoundException("id:" + userId);
+
 		return user;
 	}
-	
+
 	@DeleteMapping("/users/{userId}")
 	public void deleteUserById(@PathVariable int userId) {
-		userService.deleteUserById(userId); 
+		userService.deleteUserById(userId);
 	}
-	
+
 	// Enhancing POST Method to return correct HTTP Status Code and Location URI
 	@PostMapping("/users")
-	public ResponseEntity<UserBean> newUser(@Valid  @RequestBody UserBean user) {		
+	public ResponseEntity<UserBean> newUser(@Valid @RequestBody UserBean user) {
 		UserBean saveUser = userService.saveUser(user);
-		
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(saveUser.getId())
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(saveUser.getId())
 				.toUri();
-		return ResponseEntity.created(location).build();   
+		return ResponseEntity.created(location).build();
 	}
+
 	
-	
-	
-	
-	
+	@GetMapping("/hello")
+	public String helloUser() {
+		Locale locale = LocaleContextHolder.getLocale();  
+		return massageSource.getMessage("good.morning.message", null, "Default Message", locale);
+	}
+
 }
